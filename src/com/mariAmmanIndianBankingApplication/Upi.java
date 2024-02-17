ebb624c36fbe72d7d237b6ab7d539bae940047bb
TamilSelvan <TamilSelvan@LAPTOP-N50RULEV>
2024-02-17 07:33:55 +0530

    First Commit

---
+++ src/com/mariAmmanIndianBankingApplication/Upi.java
@@ -0,0 +1,5 @@
+package com.mariAmmanIndianBankingApplication;
+
+public class Upi {
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author TamilSelvan
+ *
+ */
+module com.mariAmmanIndianBankingApplication {
+}
